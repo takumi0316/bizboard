8c44359f69e7908e214a6a6c8b59ba301916aa19
takumi <t.okada@jiinet.co.jp>
2022-05-24 14:28:52 +0900

    modify[unicorn]

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,45 +1,46 @@
-app_path = File.expand_path('../../../', __FILE__)
-
-worker_processes 1
-# currentを指定
-working_directory "#{app_path}/current"
-
-# それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
-timeout 60
-
-#以下は応用的な設定なので説明は割愛
-
+# This loads the application in the master process before forking
+# worker processes
+# Read more about it here:
+# http://unicorn.bogomips.org/Unicorn/Configurator.html
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
-check_client_connection false
+timeout 3000
 
-run_once = true
+@app_path = '/home/media/current'
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+working_directory "#{@app_path}"
+pid "#{@app_path}/tmp/pids/unicorn.pid"
+
+# This is where we specify the socket.
+# We will point the upstream Nginx module to this socket later on
+listen "#{@app_path}/tmp/sockets/unicorn.sock", backlog: 64
+#listen "/tmp/unicorn.sock", :backlog => 64
+
+# use correct Gemfile on restarts
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{@app_path}/Gemfile"
+end
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-
-  if run_once
-    run_once = false # prevent from firing again
-  end
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
-    end
-  end
 end
 
-after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
+
+# logging
+stderr_path "log/unicorn.stderr.log"
+stdout_path "log/unicorn.stdout.log"
